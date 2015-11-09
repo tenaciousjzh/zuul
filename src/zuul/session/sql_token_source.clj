24c05f2e9dcfd30d41ea0c35d0049aeda5a36377
Jared Holmberg <jared.holmberg@gmail.com>
2015-11-09 00:09:29 -0700

    Starting to build TokenSource implementation that will be used to manage the tokens in a JDBC connected database

--- src/zuul/session/sql_token_source.clj
+++ src/zuul/session/sql_token_source.clj
@@ -1,18 +1,23 @@
 (ns zuul.session.sql-token-source
   (:require [zuul.session.token-proto :as token-proto]
+            [zuul.db.core :as db]
             [com.stuartsierra.component :as component])
   (:gen-class))
 
-(defrecord SqlTokenSource [conninfo ]
+(defrecord SqlTokenSource []
   component/Lifecycle
 
   (start [component ])
   (stop [component ])
 
   token-proto/PersistentTokenSource
+
   (get-token [component username])
   (get-user [component token])
   (remove-token [component token])
   (add-token [component token ttl-sec])
   (create-token [component ])
   (update-token-ttl [component token ttl-sec]))
+
+(defn new sql-token-source []
+  (map->SqlTokenSource {}))
